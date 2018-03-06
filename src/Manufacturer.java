389f87a9254dbc1cfa83b083a8fa39d4a284d87d
bryanhuhta <br.huhta@gmail.com>
2018-03-05 18:19:12 -0600

    Built Manufacturer.

---
+++ src/Manufacturer.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+
+public class Manufacturer implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static final String MAN_STRING = "M";
+
+    private String id;
+    private String name;
+
+    public Manufacturer(String name) {
+        this.id = MAN_STRING + (ManufacturerIdServer.instance()).getId();
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "[ id: " + id + ", name: " + name + " ]";
+    }
+}
